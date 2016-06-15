7e6ddb58d9a034a2eb08e0aa46a7c016e9d9539a
David Forsberg <david.a.forsberg@gmail.com>
2016-06-16 00:23:24 +0200

    CR part of CRUD completed; implemented dynamic ejs template

--- server.js
+++ server.js
@@ -9,27 +9,33 @@ MongoClient.connect('mongodb://<mongodb>', (err, database) => {
   if(err) return console.log(err);
   db = database;
 
+  app.set('view engine', 'ejs');
   app.use(bodyParser.urlencoded({extended: true}));
 
   app.listen(3001, function(){
-    console.log("Listening on port 3000");
-    app.get('/', (req, res) => {
-      var cursor = db.collection("quotes").find().toArray((err, results) => {
-        console.log(results);
-      });
+    console.log("Listening on port 3001");
 
-      res.sendFile(__dirname + '/index.html');
-      console.log("GET: " + __dirname);
+
+    app.get('/', (req, res) => {
+      db.collection("quotes").find().toArray((err, result) => {
+        console.log(result.length);
+        res.render('index.ejs', {quotes: result});
+      });
     });
+
 
     app.post('/quotes', (req, res) => {
       console.log(req.body);
+
       db.collection('quotes').save(req.body, (err, result) => {
+
       if (err) return console.log(err);
         console.log('Saved to database');
         res.redirect('/');
       })
     });
+
+
   });
 
 });

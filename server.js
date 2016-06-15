0293f3e20fa4985fc48a09d26c88f8e5bf8b1fd4
David Forsberg <david.a.forsberg@gmail.com>
2016-06-16 00:04:03 +0200

    MongoDB connectivity, write and read

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 const express = require('express');
-const bodyParser= require('body-parser')
+const bodyParser= require('body-parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
 
-app.use(bodyParser.urlencoded({extended: true}));
+var db
 
-app.listen(3000, function(){
-  console.log("Listening on port 3000");
-  app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-    console.log("GET: " + __dirname);
-    // Note: __dirname is the path to your current working directory. Try logging it and see what you get!
-    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+MongoClient.connect('mongodb://<mongodb>', (err, database) => {
+  if(err) return console.log(err);
+  db = database;
+
+  app.use(bodyParser.urlencoded({extended: true}));
+
+  app.listen(3001, function(){
+    console.log("Listening on port 3000");
+    app.get('/', (req, res) => {
+      var cursor = db.collection("quotes").find().toArray((err, results) => {
+        console.log(results);
+      });
+
+      res.sendFile(__dirname + '/index.html');
+      console.log("GET: " + __dirname);
+    });
+
+    app.post('/quotes', (req, res) => {
+      console.log(req.body);
+      db.collection('quotes').save(req.body, (err, result) => {
+      if (err) return console.log(err);
+        console.log('Saved to database');
+        res.redirect('/');
+      })
+    });
   });
 
-  app.post('/quotes', (req, res) => {
-    console.log('Hellooooooooooooooooo!')
-  })
 });
